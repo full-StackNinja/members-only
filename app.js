ad91e5cc67924b0157053ce6c9cf1cb68e3b5b07
Imran Hussain <imranhussain399@gmail.com>
2024-02-10 11:28:37 +0500

    Remove passport and session implementation

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const logger = require("morgan");
 const nconf = require("nconf");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const passport = require("./server/passport");
+const sessionMiddleware = require("./server/session");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
+// Load env variables
 nconf.env();
 
 const main = async () => {
@@ -20,7 +23,14 @@ const main = async () => {
 main().catch((err) => {
   console.log(err);
 });
+
 const app = express();
+
+app.use(sessionMiddleware);
+
+// Initialize and use passport session to manage sessions
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -33,7 +43,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/user", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
