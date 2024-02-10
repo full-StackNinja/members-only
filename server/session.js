9b335acab5e4b6aa7e3877bfe3a4d0965ff7fab8
Imran Hussain <imranhussain399@gmail.com>
2024-02-10 11:26:11 +0500

    Define session

---
+++ server/session.js
@@ -0,0 +1,9 @@
+const session = require("express-session");
+
+const sessionMiddleware = session({
+  secret: "cat",
+  resave: false,
+  saveUninitialized: true,
+});
+
+module.exports = sessionMiddleware;
