736aba5195aec21c15ce5b9da097235c84aec68b
Imran Hussain <imranhussain399@gmail.com>
2024-02-10 11:25:13 +0500

    Define user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const { route } = require(".");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/home", userController.home);
+
+router.get("/login", userController.login_get);
+router.post("/login", userController.login_post);
+
+router.get("/log-out", userController.log_out_get);
+
+router.get("/sign-up", userController.sign_up_get);
+router.post("/sign-up", userController.sign_up_post);
+
+router.get("/join-club", userController.join_club_get);
+router.post("/join-club", userController.join_club_post);
 
 module.exports = router;
