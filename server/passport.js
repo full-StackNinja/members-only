fc387c4e2474e9374c917030522991e56c9ad3fb
Imran Hussain <imranhussain399@gmail.com>
2024-02-10 11:25:54 +0500

    Define passport local strategy

---
+++ server/passport.js
@@ -0,0 +1,42 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const User = require("../server/../models/User");
+
+// Define passport authentication strategy
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect user name" });
+      }
+      if (!await bcrypt.compare(password, user.password)) {
+        return done(null, false, { message: "Incorrect user password" });
+      } else {
+        return done(null, user);
+      }
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+// Serialize user
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Deserialize user
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+module.exports = passport;
