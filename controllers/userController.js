25ed66642002ccd0604774f615209f8c13221f03
Imran Hussain <imranhussain399@gmail.com>
2024-02-11 22:41:09 +0500

    Implement route handler functions for home page, join club, admin login and deleting messages routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,22 @@ const User = require("../models/User");
 const Message = require("../models/Message");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const nconf = require("nconf");
 const passport = require("../controllers/../server/passport");
 const { body, validationResult } = require("express-validator");
 
 exports.home = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented: home route");
+  if (req.isAuthenticated()) {
+    const message_list = await Message.find({})
+      .populate("user")
+      .sort({ timestamp: 1 })
+      .exec();
+    // console.log("req.user in home route", req.user);
+    // console.log("req.isAthenticated()", req.isAuthenticated());
+    res.render("home", { message_list });
+  } else {
+    res.redirect("/user/login?status=Please login first");
+  }
 });
 
 exports.login_get = asyncHandler(async (req, res, next) => {
@@ -101,8 +112,80 @@ exports.sign_up_post = [
 ];
 
 exports.join_club_get = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented: join club get");
+  console.log("req.user in join club route", req.user);
+  console.log("req.isAthenticated()", req.isAuthenticated());
+  res.render("join-club-form", { title: "Join Private Club" });
 });
-exports.join_club_post = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented: join club post");
+exports.join_club_post = [
+  body("code")
+    .trim()
+    .custom((value) => {
+      const code = nconf.get("CLUB_CODE");
+      return value === code;
+    })
+    .withMessage("Incorrect code"),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("join-club-form", {
+        title: "Join Private Club",
+        errors: errors.array(),
+      });
+      return;
+    }
+    console.log("req.user before", req.user);
+    const user = await User.findOne({ username: req.user.username });
+
+    if (user) {
+      user.membership = true;
+      await user.save();
+      console.log("user from database", user);
+      console.log("req.user after", req.user);
+      res.redirect("/user/home");
+    } else {
+      const err = new Error("User does not exist");
+      next(err);
+    }
+  }),
+];
+
+exports.admin_login_get = asyncHandler(async (req, res, next) => {
+  res.render("admin-form", { title: "Admin Login Form" });
+});
+exports.admin_login_post = [
+  body("code")
+    .trim()
+    .custom((value) => {
+      const code = nconf.get("ADMIN_CODE");
+      return value === code;
+    })
+    .withMessage("Incorrect code"),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("admin-form", {
+        title: "Admin Login Form",
+        errors: errors.array(),
+      });
+      return;
+    }
+    console.log("req.user before", req.user);
+    const user = await User.findOne({ username: req.user.username });
+
+    if (user) {
+      user.isAdmin = true;
+      await user.save();
+      console.log("user from database", user);
+      console.log("req.user after", req.user);
+      res.redirect("/user/home");
+    } else {
+      const err = new Error("User does not exist");
+      next(err);
+    }
+  }),
+];
+
+exports.delete_message = asyncHandler(async (req, res, next) => {
+  const message = await Message.findByIdAndDelete(req.params.id);
+  res.redirect("/user/home");
 });
