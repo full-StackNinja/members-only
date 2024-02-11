1291295b22718096823f157635f4085794c4d2bc
Imran Hussain <imranhussain399@gmail.com>
2024-02-11 22:44:14 +0500

    Add routes for creating new message, admin login and deleting message

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const { route } = require(".");
+const messageController = require("../controllers/messageController");
 
 router.get("/home", userController.home);
 
 router.get("/login", userController.login_get);
 router.post("/login", userController.login_post);
+
+router.get("/create-message", messageController.create_message_get);
+router.post("/create-message", messageController.create_message_post);
 
 router.get("/log-out", userController.log_out_get);
 
@@ -15,5 +18,10 @@ router.post("/sign-up", userController.sign_up_post);
 
 router.get("/join-club", userController.join_club_get);
 router.post("/join-club", userController.join_club_post);
+
+router.get("/admin-login", userController.admin_login_get);
+router.post("/admin-login", userController.admin_login_post);
+
+router.get("/message/:id/delete", userController.delete_message);
 
 module.exports = router;
